c6cf1b7aea4e79d971798f64195d918a282bd933
Karlo1209 <karlo.mikovic@student.uhasselt.be>
2025-03-06 14:58:56 +0100

    model 5

--- Code.R
+++ Code.R
@@ -37,29 +37,28 @@ model2 <- lm(lnEnergy ~ lnER + lnPcca + lnDa + lnSize + lnAge + Own + Export
 summary(model2)
 model2_RSE <- coeftest(model2, vcov. = vcovHC, type = "HC1")
 
-##Regression model 3 with control variables and industry FE
-model3 <- plm(lnEnergy ~ lnER + lnPcca + lnDa + lnSize + lnAge + Own + Export
-              + lnOpen + Ind + Endowment + Rail + lnPcgdp + Concentration, 
-              data = raw_data,
-              index = "ind_final",
-              model = "within")
-summary(model3)
-model3_RSE <- coeftest(model3, vcov. = vcovHC, type = "HC1")
-
-###Regression model 3 using manually demeaning (not optimal yet)
+##Regression model 3 with control variables and industry FE (using manually demeaning) 
 dataset_model3 <- group_by(raw_data, ind_final)
-lnER_means <- summarise(dataset_model3, mean(lnER, na.rm = T))
-lnEnergy_means <- summarise(dataset_model3, mean(lnEnergy, na.rm = T))
+lnER_means <- summarise(dataset_model3, mean_lnER = mean(lnER, na.rm = TRUE))
+lnEnergy_means <- summarise(dataset_model3, mean_lnEnergy = mean(lnEnergy, na.rm = TRUE))
 
 dataset_model3_demeaned <- raw_data
-for (i in 1:dim(lnER_means)[1]) {
-  posi <- which(raw_data$ind_final == lnER_means$ind_final[i])
-  dataset_model3_demeaned[posi, "lnER"] <- raw_data[posi, "lnER"] - as.numeric(lnER_means[i,2])
-  dataset_model3_demeaned[posi, "lnEnergy"] <- raw_data[posi, "lnEnergy"] - as.numeric(lnEnergy_means[i,2])}
 
-coeftest(lm(lnEnergy ~ lnER + lnPcca + lnDa + lnSize + lnAge + Own + Export
-            + lnOpen + Ind + Endowment + Rail + lnPcgdp + Concentration
-            , dataset_model3_demeaned), vcov. = vcovHC, type = "HC1")
+for (i in 1:nrow(lnER_means)) {
+  posi <- which(raw_data$ind_final == lnER_means$ind_final[i])
+  dataset_model3_demeaned[posi, "lnER"] <- raw_data[posi, "lnER"] - lnER_means$mean_lnER[i]
+  dataset_model3_demeaned[posi, "lnEnergy"] <- raw_data[posi, "lnEnergy"] - lnEnergy_means$mean_lnEnergy[i]
+}
+
+raw_data$ind_final <- factor(raw_data$ind_final)
+
+model <- lm(lnEnergy ~ lnER + lnPcca + lnDa + lnSize + lnAge + Own + Export
+            + lnOpen + Ind + Endowment + Rail + lnPcgdp + Concentration + 
+              factor(ind_final), 
+            data = raw_data)
+
+robust_se <- vcovHC(model, type = "HC1")
+model3_RSE <- coeftest(model, vcov. = robust_se)
 
 ##Regression model 4 with control variables, year FE and firm FE
 model4 <- plm(lnEnergy ~ lnER + lnPcca + lnDa + lnSize + lnAge + Own + Export
@@ -70,17 +69,33 @@ model4 <- plm(lnEnergy ~ lnER + lnPcca + lnDa + lnSize + lnAge + Own + Export
 summary(model4)
 model4_RSE <- coeftest(model4, vcov. = vcovHC, type = "HC1")
 
-##Regression model 5 with control variables and all FE
-model5 <- plm(lnEnergy ~ lnER + lnPcca + lnDa + lnSize + lnAge + Own + Export
-              + lnOpen + Ind + Endowment + Rail + lnPcgdp + Concentration, 
-              data = raw_data,
-              index = c("ind_final", "id_in_panel", "year"),
-              model = "within")
-summary(model5)
-model5_RSE <- coeftest(model5, vcov. = vcovHC, type = "HC1") 
+##Regression model 5 with control variables and all FE (industry FE, year FE and firm FE) --> Error: cannot allocate vector of size 44.6 Gb
+dataset_model5 <- group_by(raw_data, ind_final)
+lnER_means <- summarise(dataset_model5, ind_final = unique(ind_final), mean_lnER = mean(lnER, na.rm = TRUE))
+lnEnergy_means <- summarise(dataset_model5, ind_final = unique(ind_final), mean_lnEnergy = mean(lnEnergy, na.rm = TRUE))
+dataset_model5_demeaned <- raw_data
+
+for (i in 1:nrow(lnER_means)) {
+  posi <- which(raw_data$ind_final == lnER_means$ind_final[i])
+  dataset_model5_demeaned$lnER[posi] <- raw_data$lnER[posi] - lnER_means$mean_lnER[i]
+  dataset_model5_demeaned$lnEnergy[posi] <- raw_data$lnEnergy[posi] - lnEnergy_means$mean_lnEnergy[i]
+}
+
+raw_data$ind_final <- factor(raw_data$ind_final)  
+raw_data$year <- factor(raw_data$year)            
+raw_data$id_in_panel <- factor(raw_data$id_in_panel)      
+
+model <- lm(lnEnergy ~ lnER + lnPcca + lnDa + lnSize + lnAge + Own + Export
+            + lnOpen + Ind + Endowment + Rail + lnPcgdp + Concentration + 
+              factor(ind_final) + factor(year) + factor(id_in_panel), 
+            data = raw_data)
+
+robust_se <- vcovHC(model, type = "HC1")
+model5_RSE <- coeftest(model, vcov. = robust_se)
+
 
 ##Overview of all models with RSE
-stargazer(model1_RSE, model2_RSE, model3_RSE, model4_RSE, model5_RSE, type = "html", title = "Benchmark regression results.", digits = 3, out = "Benchmark_regression_results.html")
+stargazer(model1_RSE, model2_RSE, model3_RSE, model4_RSE, type = "html", title = "Benchmark regression results.", digits = 3, out = "Benchmark_regression_results.html")
 
 
 
